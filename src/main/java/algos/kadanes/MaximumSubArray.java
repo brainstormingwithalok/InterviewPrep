6a396971f1cbfb3157be708e51f249dd9aebe907
Alok Mishra <alokmishra@Aloks-MacBook-Pro.local>
2023-12-01 21:10:10 +0530

    first version

---
+++ src/main/java/algos/kadanes/MaximumSubArray.java
@@ -0,0 +1,40 @@
+package main.java.algos.kadanes;
+
+/**
+ * Given an integer array nums, find the subarray with the largest sum, and return its sum
+ * Example 1:
+ * <p>
+ * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * Output: 6
+ * Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+ * Example 2:
+ * <p>
+ * Input: nums = [1]
+ * Output: 1
+ * Explanation: The subarray [1] has the largest sum 1.
+ * Example 3:
+ * <p>
+ * Input: nums = [5,4,-1,7,8]
+ * Output: 23
+ * Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ */
+public class MaximumSubArray {
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(arr));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum = currentSum + nums[i];
+            maxSum = Math.max(currentSum, maxSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
