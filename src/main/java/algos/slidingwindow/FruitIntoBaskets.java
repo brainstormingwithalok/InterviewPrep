19bcb2ea3541ca82ad0c3507f6a0578d3444f395
Alok Mishra <alokmishra@Aloks-MacBook-Pro.local>
2023-12-12 18:22:44 +0530

    binary search

---
+++ src/main/java/algos/slidingwindow/FruitIntoBaskets.java
@@ -0,0 +1,65 @@
+package main.java.algos.slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+ * <p>
+ * You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+ * <p>
+ * You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+ * Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+ * Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+ * Given the integer array fruits, return the maximum number of fruits you can pick.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: fruits = [1,2,1]
+ * Output: 3
+ * Explanation: We can pick from all 3 trees.
+ * Example 2:
+ * <p>
+ * Input: fruits = [0,1,2,2]
+ * Output: 3
+ * Explanation: We can pick from trees [1,2,2].
+ * If we had started at the first tree, we would only pick from trees [0,1].
+ * Example 3:
+ * <p>
+ * Input: fruits = [1,2,3,2,2]
+ * Output: 4
+ * Explanation: We can pick from trees [2,3,2,2].
+ * If we had started at the first tree, we would only pick from trees [1,2].
+ */
+public class FruitIntoBaskets {
+    public static void main(String[] args) {
+        int[] fruits = {1, 2, 3, 2, 2};
+        System.out.println(totalFruit(fruits));
+    }
+
+    public static int totalFruit(int[] fruits) {
+        // Hash map 'basket' to store the types of fruits.
+        Map<Integer, Integer> basket = new HashMap<>();
+        int left = 0, right;
+
+        // Add fruit from right side (right) of the window.
+        for (right = 0; right < fruits.length; ++right) {
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // If the current window has more than 2 types of fruit,
+            // we remove one fruit from the left index (left) of the window.
+            if (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+                if (basket.get(fruits[left]) == 0)
+                    basket.remove(fruits[left]);
+                left++;
+            }
+        }
+
+        // Once we finish the iteration, the indexes left and right
+        // stands for the longest valid subarray we encountered.
+        return right - left;
+    }
+}
